f85bac86e587ff1b66c5b9a0741cb7ab9b93de31
Hadi <hdi.amiri@gmail.com>
2024-03-22 18:50:54 -0400

    Add Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'material-todo-app';
+
+   todoItems = [
+    {
+      description: 'Todo 1',
+      createDate: new Date(),
+      done: false
+    },
+    {
+      description: 'Todo 2',
+      createDate: new Date(),
+      done: true
+    }
+  ]
+
 }
