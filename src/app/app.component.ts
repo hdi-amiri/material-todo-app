20d7bd4b6885ebbb84c77e66e9f6e992fa174465
Hadi <hdi.amiri@gmail.com>
2024-03-22 20:54:19 -0400

    Fix theme issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,29 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from "@angular/material/checkbox";
+
+import {Todo} from "./todo";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [FormsModule, MatCheckboxModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
-   todoItems = [
+   todoItems: Todo[] = [
     {
       description: 'Todo 1',
-      createDate: new Date(),
+      createDate: new Date().getTime(),
       done: false
     },
     {
       description: 'Todo 2',
-      createDate: new Date(),
+      createDate: new Date().getTime(),
       done: true
     }
   ]
-
 }
